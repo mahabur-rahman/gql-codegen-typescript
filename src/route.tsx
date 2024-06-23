d67de80e67fb0fef0018f4b3b2f9cc99515cc356
mahabur-rahman <annur4395@gmail.com>
2024-06-24 05:42:53 +0600

    profile page

--- src/route.tsx
+++ src/route.tsx
@@ -1,6 +1,7 @@
 
 import {Home} from "./pages/Home";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import { QuotePage } from "./pages/QuotePage";
 import Signup from "./pages/Signup";
 
@@ -9,6 +10,6 @@ export const routes = [
   { path: "/signup", element: <Signup /> },
   { path: "/signin", element: <Login /> },
   { path: "/quotes", element: <QuotePage /> },
-//   { path: "/profile", element: <Profile /> },
+  {path: '/profile', element: <Profile />}
 //   { path: "/create-quote", element: <CreateQuote /> },
 ];
