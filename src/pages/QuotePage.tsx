ac78e6bc1128b3165633e283c4f06868215c97ab
mahabur-rahman <annur4395@gmail.com>
2024-06-24 06:55:20 +0600

    search functionality added

--- src/pages/QuotePage.tsx
+++ src/pages/QuotePage.tsx
@@ -49,7 +49,7 @@ const QuotePage = () => {
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-          Quote Page
+          Quote Page ---- {data?.getAllQuotes.length} results
         </h2>
       </div>
       <div className="grid gap-8 row-gap-10 lg:grid-cols-2">
