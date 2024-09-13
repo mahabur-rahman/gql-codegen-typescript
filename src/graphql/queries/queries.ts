8e666a412d20201363fbfaec234040837a3c3efa
mahabur-rahman <annur4395@gmail.com>
2024-09-13 18:35:53 +0600

    fix: Get all quotes query with gql

--- src/graphql/queries/queries.ts
+++ src/graphql/queries/queries.ts
@@ -1,37 +1,34 @@
 import { gql } from "../__generated__";
 
 export const GET_ALL_QUOTES = gql(`
-   query getAllQuotes($title:String) {
-    getAllQuotes(title: $title) {
-        _id
-        title
-        images
-        videos
-        rating
-        createBy {
-            _id
-            firstName
-            lastName
-            email
-            role
-        }
-      likes{
-        _id
-        firstName
-        lastName
-        email
-        role
-      }
-      dislikes{
-          _id
-        firstName
-        lastName
-        email
-        role
-
-      }
+  query GetAllQuotes($filters: QuoteFiltersInput) {
+  getAllQuotes(filters: $filters) {
+    _id
+    title
+    createBy {
+      _id
+      firstName
+      lastName
+      email
+      role
     }
+    likes {
+      _id
+      firstName
+      lastName
+      email
+      role
+    }
+    dislikes {
+      _id
+      firstName
+      lastName
+      email
+      role
+    }
+  }
 }
+
 `);
 
 export const GET_COMMENT_QUOTE_REF = gql(`
